e8b33eea7bde603676894b83f00a47667508aaa0
Nyawera-Tut <nyaweratutchuol@gmail.com>
2022-02-27 16:21:38 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,32 @@
+fun main(){
+    var campus = "akirachix"
+    var cmpsd = campus[0]
+    var cmpsd1= campus[2]
+    var cmpsd2 = campus[3]
+    var result = ("$cmpsd$cmpsd1$cmpsd2")
+    println (result)
+
+
+    var x = "Nyawera"
+    var y = 16
+    var state = ("Hi,my name is $x and i am $y years old")
+    println(state)
+
+    var fruit = "watermelon"
+    println(fruit.length)
+
+    var name = "Nyawera"
+    if( name == "Nyawera"){
+        println("Thats me!")
+
+ }else {
+    println("I don't know who that is")
+ }
+}
+
+
+//fun create(name:String,age:Int):String{
+
+
+
+//}
